79bfab21f65acfe9288382f325d0fb56346e93a8
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-06-04 20:27:08 +0000

    add flexible dice pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,17 @@ class HomeController < ApplicationController
   def load_home
     render({:template => "dice_pages/dice_home"})
   end
+
+  def load_dice
+    @num_dice = params.fetch("number_of_dice").to_i
+    @sides = params.fetch("how_many_sides").to_i
+    @rolls = []
+
+    @num_dice.times do
+      die = rand(1..@sides)
+      @rolls.push(die)
+    end
+
+    render({:template => "dice_pages/dice_dynamic"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 
-  get("/", { :controller => "home", :action => "load_home"})
+  get("/", { :controller => "home", :action => "load_home" })
+
+  get("/dice/:number_of_dice/:how_many_sides", { :controller => "home", :action => "load_dice" })
 end
